50ba26cb0699fb55d46d94a5640b94fd97b29628
Sandy Angelica Perez Espinoza <sandy.perez@neoris.com>
2023-03-03 16:29:48 -0600

    subir proyecto

--- demo/src/main/java/com/example/demo/security/AuthorizationServerConfig.java
+++ demo/src/main/java/com/example/demo/security/AuthorizationServerConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo.security;
 
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
